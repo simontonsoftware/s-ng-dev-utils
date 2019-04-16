491285e5e53811cbcea6ada81278045d9d2ec899
ersimont <8042088+ersimont@users.noreply.github.com>
2019-04-15 22:43:34 -0400

    chore: remove scaffolded library code

--- projects/s-ng-test-utils/src/lib/s-ng-test-utils.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { SNgTestUtilsComponent } from "./s-ng-test-utils.component";
-
-@NgModule({
-  declarations: [SNgTestUtilsComponent],
-  imports: [],
-  exports: [SNgTestUtilsComponent],
-})
-export class SNgTestUtilsModule {}
--- projects/s-ng-test-utils/src/public-api.ts
+++ projects/s-ng-test-utils/src/public-api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of s-ng-test-utils
  */
-
-export * from "./lib/s-ng-test-utils.service";
-export * from "./lib/s-ng-test-utils.component";
-export * from "./lib/s-ng-test-utils.module";
