7974b57f572ef190c9eab089866aa304c5edbbd6
ersimont <8042088+ersimont@users.noreply.github.com>
2019-06-15 18:23:07 -0400

    chore: document `marbleTest()`

--- projects/s-ng-dev-utils/src/lib/marble-test.ts
+++ projects/s-ng-dev-utils/src/lib/marble-test.ts
@@ -13,6 +13,17 @@ type Callback<T> = (
   testHelpers: Helpers & { testScheduler: TestScheduler },
 ) => T;
 
+/**
+ * A helper to take some boilerplate out of RxJS's new marble testing API for jasmine.
+ *
+ * ```ts
+ * it("doubles numbers", marbleTest(({ cold, expectObservable }) => {
+ *   const source = cold("-1-2-3-|");
+ *   const expected = "   -2-4-6-|");
+ *   expectObservable(source.pipe(double())).toBe(expected);
+ * }));
+ * ```
+ */
 export function marbleTest<T>(callback: Callback<T>) {
   const testScheduler = new TestScheduler((actual, expected) => {
     expect(actual).toEqual(expected);
