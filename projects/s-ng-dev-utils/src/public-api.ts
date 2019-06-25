a6c0f7589fa7e55b4db8dbc9a07868b8b2b159c1
ersimont <8042088+ersimont@users.noreply.github.com>
2019-06-25 06:31:22 -0400

    chore: Remove utility types.
    BREAKING CHANGE: Removes all exported types from this library. They did not fit the spirit of "dev" utils, because they made this library a real dependency of libraries that use it

--- projects/s-ng-dev-utils/src/public-api.ts
+++ projects/s-ng-dev-utils/src/public-api.ts
@@ -6,4 +6,3 @@ export { expectCallsAndReset } from "./lib/expect-calls-and-reset";
 export { expectSingleCallAndReset } from "./lib/expect-single-call-and-reset";
 export { expectType } from "./lib/expect-type";
 export { marbleTest } from "./lib/marble-test";
-export * from "./lib/utility-types";
--- src/app/integration.spec.ts
+++ src/app/integration.spec.ts
@@ -1,20 +1,8 @@
 import {
-  Existant,
   expectCallsAndReset,
   expectSingleCallAndReset,
   expectType,
-  Falsey,
-  Function0,
-  Function1,
-  Function2,
-  Function3,
-  Function4,
   marbleTest,
-  Nil,
-  NumberKeyedObject,
-  ObjectWith,
-  Primitive,
-  Transformer,
 } from "s-ng-dev-utils";
 
 describe("s-ng-dev-utils", () => {
@@ -40,25 +28,4 @@ describe("s-ng-dev-utils", () => {
       expectObservable(cold("|")).toBe("|");
     }),
   );
-
-  it("has utility types", () => {
-    expectType<Primitive>(9);
-    expectType<Existant>(0);
-    expectType<Nil>(null);
-    expectType<Falsey>(0);
-    expectType<ObjectWith<number>>({ a: 1 });
-    expectType<NumberKeyedObject>({ 1: "a" });
-    expectType<Function0<number>>(() => 0);
-    expectType<Function1<number, number>>((n1: number) => n1);
-    expectType<Function2<number, number, number>>(
-      (n1: number, n2: number) => n1 + n2,
-    );
-    expectType<Function3<number, number, number, number>>(
-      (n1: number, n2: number, n3: number) => n1 + n2 + n3,
-    );
-    expectType<Function4<number, number, number, number, number>>(
-      (n1: number, n2: number, n3: number, n4: number) => n1 + n2 + n3 + n4,
-    );
-    expectType<Transformer<number>>((num: number) => num + 1);
-  });
 });
